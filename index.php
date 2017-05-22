05763fe58351381739e592a11c67c3e02764ac3d
sergeykariy55 <sergey_kariy_55@mail.ru>
2017-05-22 21:26:21 +0300

    создана главная страница, навешаны стили

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 ﻿<?php
     session_start();
-    include "config/config.php";
+    include "admin/config.php";
+    $id = $_GET['id'];
 ?>
 <html>
 <head>
@@ -10,44 +11,57 @@
 <div class="container">
 <ul class=menu_g>
 	<li><span class=ad><a href=index.php class="c1">Главная</a></span></li>
-	<li><a href=cat.php class=a_click>Категории</a></li>
 	<li><a href=stat.php class=a_click>Статьи</a></li>
 	<li><a href=users.php class=a_click>Пользователи</a></li>
-	<li><a href=author.php class=a_click>Авторы</a></li>
-        <li><a href=auth.php class=a_click>Войти</a></li>
+    <li><a href=auth.php class=a_click>Войти</a></li>
 </ul>
-
-<div class="left_b">
-<h2>Меню</h2>
-<ul class="rubi">
-<?php
-	foreach($dbh->query('SELECT * FROM category') as $row){
-	?>
-	<li><a href=cat.php?id=<?php echo $row['id'] ?> class=fwf><?php echo $row['name_cat']; ?></a></li>
-	<?php
-	}
-?>
-</ul>
+<div class="info">
+	<p>LMS - система для удаленного обучения</p>
+	<img src=/img/lms-header.png class="img_info">
 </div>
-
-<div class="right_b">
-<h2>Последние статьи</h2>
-
-<?php
-	foreach($dbh->query('SELECT * from items') as $row){
-	?>
-	<div class="fvd">
-	<p><?php echo $row["content"]; ?></p>
-	<br>
-	<div class="ew3">
-	<a href=reader.php?id=<?php echo $row['id'] ?> class=fwf>Читать</a>
-	</div>
-	</div>
+	<div class="left_b">
+	<h2>Последние занятия</h2>
+	<ul class="rubi">
 	<?php
-	}
-?>
+		foreach($dbh->query('SELECT * FROM `events` ORDER BY `events`.`id` DESC LIMIT 5') as $row){
+		?>
+			<div class="message">
+				<div class="index_user">
+					<div class="img_user">
+						<img src=/img/head_user.png class="img_user">
+					</div>
+					<div class="content_user">
+						<div class="name_user"><p><?=$row['title'];?></p></div>
+						<div class="data"><?=$row['content'];?></div>
+					</div>
+				</div>		
+			</div>
+		<?php
+		}
+	?>
+	</ul>
+	</div>
 
-</div>
+	<div class="right_b">
+	<h2>Новые пользователи</h2>
+
+	<?php
+		foreach($dbh->query('SELECT * FROM `users` ORDER BY `users`.`id` DESC') as $row){
+		?>
+			<div class="message">
+				<div class="img_user_lenta">
+						<img src=/img/user.png class="img_user_lenta">
+					</div>
+				<div class="content_user_lenta">
+					<div class="name_user"><p><?=$row['fio'];?></p></div>
+					<div class="data"><?=$row['adout'];?></div>
+				</div>		
+			</div>
+		<?php
+		}
+	?>
+
+	</div>
 </div>
 </body>
 </html>
